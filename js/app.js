fe15a0729bc34244dfd278184019d5097c04c327
Willy Aguirre <willy@willy-K52Dr.(none)>
2012-10-10 19:03:55 -0500

    listogit add .

---
+++ js/app.js
@@ -0,0 +1,40 @@
+$('#buscar').submit(function() {	
+	$('#res').html("");
+	var b = $("#txtbuscar").val();
+	$.getJSON('https://support.mozilla.org/es/search?q=' + b + '&format=json&callback=?',function(data) {		 
+
+		$.each(data, function(key, val) {		    
+		    $.each(val, function(key2, val2) {
+			    	if (typeof val2.title == "undefined"){										  
+					}else {						
+						$('#res').append('<li>' + val2.title + '</li>');					
+					}
+					
+		  	});
+		  	if(val == 0 ){
+		  		$('#res').html("no hay ninguna referencia");
+		  	} 
+		});
+	});	
+});
+
+$('#btnbuscar').click(function() {
+	$('#res').html("");
+	var b = $("#txtbuscar").val();
+	$.getJSON('https://support.mozilla.org/es/search?q=' + b + '&format=json&callback=?',function(data) {		 
+
+		$.each(data, function(key, val) {		    
+		    $.each(val, function(key2, val2) {
+		    	if (typeof val2.title == "undefined")
+				{										  
+				}else {
+					
+					$('#res').append('<li>' + val2.title + '</li>');					
+				}		    	
+		  	});
+		  	if(val == 0 ){
+		  		$('#res').html("no hay ninguna referencia");
+		  	} 
+		});
+	});	
+});
