f329ad63d83aed3cdaba609fec731f867bfedb17
Willy Aguirre <willy@willy-K52Dr.(none)>
2012-10-26 23:46:13 -0500

    listo y en Heroku

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	$('.mensaje').hide();
 	$('.mensaje_error').hide();
-	//$('#res').hide();
+	$('#res').hide();
 
 	$('div#form form').submit(function () {
 
@@ -14,20 +14,21 @@ $(document).ready(function() {
 
 	    }
 
-	    $('#res').html("");
+	    $('#holder').html("");
 	    var b = $("#txtbuscar").val();
 	    $.getJSON('https://support.mozilla.org/es/search?q=' + b + '&format=json&callback=?', function (data) {
 
 	        if (typeof data.message == "undefined") {
 	            $.each(data, function (key, val) {
 	                $.each(val, function (key2, val2) {
-	                    if (typeof val2.title == "undefined") {} else {	                    	
-	                        //$('#res').append('<li>' + val2.title + '</li>');
-	                        var titulo = $('<a href=https://support.mozilla.org' + val2.url +' target=_blank>' + val2.title + '</li><br/>');
-	                        var descripcion = $('<span>' + val2.search_summary + '</span><br/><br/>');
+	                    if (typeof val2.title == "undefined") {} else if(val2.type == "document"){	                    	
+	                        $('#res').show();
+	                        var titulo = $('<li><a href=https://support.mozilla.org' + val2.url +' target=_blank>' + val2.title + '</a></li>');
+	                        var descripcion = $('<li>' + val2.search_summary + '</li>');
 
-	                        $("#res").append(titulo);
-	                        $("#res").append(descripcion);
+	                        $("#holder").append(titulo);
+	                        $("#holder").append(descripcion);
+	                        $("ul.pagination").quickPagination();
 	                    }
 	                });
 	            });
@@ -37,39 +38,38 @@ $(document).ready(function() {
 	            $('.mensaje').fadeIn(100);
 	            $('.mensaje').html("no hay ninguna referencia");
 	            $('.mensaje').delay(2000).fadeOut('slow');
+	            $('#res').hide();
 	        }
 
-	    });
-
-	    //$("#form").animate({opacity: 'hide'}, 300);
-	    //$("#res").animate({opacity: 'show'}, 300);
-
+	    });		
 
 	});
 
 	$('#btnbuscar').click(function () {
 
-    if ($('#txtbuscar').val().length < 1) {
-        $('.mensaje').hide();
-        $('.mensaje_error').fadeIn(100);
-        $('.mensaje_error').html("Por favor, rellene este campo.");
-        $('.mensaje_error').delay(2000).fadeOut('slow');
+	    if ($('#txtbuscar').val().length < 1) {
+	        $('.mensaje').hide();
+	        $('.mensaje_error').fadeIn(100);
+	        $('.mensaje_error').html("Por favor, rellene este campo.");
+	        $('.mensaje_error').delay(2000).fadeOut('slow');
 
-    }
+	    }
 
-    $('#res').html("");
+    $('#holder').html("");
     var b = $("#txtbuscar").val();
 	    $.getJSON('https://support.mozilla.org/es/search?q=' + b + '&format=json&callback=?', function (data) {
 
 	        if (typeof data.message == "undefined") {
 	            $.each(data, function (key, val) {
 	                $.each(val, function (key2, val2) {
-	                    if (typeof val2.title == "undefined") {} else {
-	                        var titulo = $('<a href=https://support.mozilla.org' + val2.url +' target=_blank>' + val2.title + '</li><br/>');
-	                        var descripcion = $('<span>' + val2.search_summary + '</span><br/><br/>');
+	                    if (typeof val2.title == "undefined") {} else if(val2.type == "document"){	                    	
+	                        $('#res').show();
+	                        var titulo = $('<li><a href=https://support.mozilla.org' + val2.url +' target=_blank>' + val2.title + '</a></li>');
+	                        var descripcion = $('<li>' + val2.search_summary + '</li>');
 
-	                        $("#res").append(titulo);
-	                        $("#res").append(descripcion);
+	                        $("#holder").append(titulo);
+	                        $("#holder").append(descripcion);
+	                        $("ul.pagination").quickPagination();
 	                    }
 	                });
 	            });
@@ -79,15 +79,11 @@ $(document).ready(function() {
 	            $('.mensaje').fadeIn(100);
 	            $('.mensaje').html("no hay ninguna referencia");
 	            $('.mensaje').delay(2000).fadeOut('slow');
+	            $('#res').hide();
 	        }
 
 	    });
 
-	    //$("#form").animate({opacity: 'hide'}, 300);
-	    //$("#res").animate({opacity: 'show'}, 300);
-
 	});
-	
+
 });
-
-
